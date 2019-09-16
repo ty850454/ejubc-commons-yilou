ff512141a284f70f3ed8bee16f534e842d6ec088
徐杨 <ty850454@126.com>
2019-09-16 10:36:43 +0800

    feat:redis分布式锁改造

---
+++ src/main/java/com/ejubc/commons/yilou/component/Lock.java
@@ -0,0 +1,26 @@
+package com.ejubc.commons.yilou.component;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/**
+ * 锁
+ * @author xy
+ */
+@Setter
+@Getter
+public class Lock {
+
+    /** 锁的key */
+    private String key;
+    /** 锁的value，方式其它请求释放锁 */
+    private String value;
+
+    public Lock(String key) {
+        this.key = key;
+        this.value = UUID.randomUUID().toString();
+    }
+}
--- src/main/java/com/ejubc/commons/yilou/component/RedisLock.java
+++ src/main/java/com/ejubc/commons/yilou/component/RedisLock.java
@@ -7,11 +7,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.connection.RedisStringCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
+import java.util.Collections;
 
 /**
  * Redis分布式锁
@@ -35,18 +38,19 @@ public class RedisLock {
         this.template = template;
     }
 
+
     /**
      * 获取锁
      *
-     * @param key 锁的名称KEY
+     * @param lock 锁
      * @param timeout 累计获取锁的总时间达到多少后不再尝试，即超时时间（单位毫秒，默认3秒）
      * @param tryInterval 每隔多少时间重新尝试一次获取（单位毫秒，默认200毫秒）
      * @param lockExpireTime 获取锁成功后，锁的过期时间（单位毫秒，默认30秒）
      * @return 成功失败
      */
-    public boolean tryLock(String key, Long timeout, Long tryInterval, Long lockExpireTime) {
-        log.debug("获取锁,key={}", key);
-        if (StringUtils.isEmpty(key)) {
+    public boolean tryLock(Lock lock, Long timeout, Long tryInterval, Long lockExpireTime) {
+        log.debug("获取锁,key={}", lock.getKey());
+        if (StringUtils.isEmpty(lock.getKey())) {
             return false;
         }
         if (timeout == null) {
@@ -60,7 +64,7 @@ public class RedisLock {
         }
         try {
             long startTime = System.currentTimeMillis();
-            for (;!coreLock(key, lockExpireTime);) {
+            for (; !coreLock(lock, lockExpireTime);) {
                 if (System.currentTimeMillis() - startTime > timeout) {
                     return false;
                 }
@@ -68,7 +72,7 @@ public class RedisLock {
             }
             return true;
         } catch (InterruptedException e) {
-            log.error("获取锁失败,key={}", key, e);
+            log.error("获取锁失败,key={}", lock.getKey(), e);
         }
         return false;
     }
@@ -84,21 +88,36 @@ public class RedisLock {
      * @return 成功失败
      */
     public boolean tryLock(String key, Long timeout, Long tryInterval, Long lockExpireTime, IProcess iProcess) {
+        return tryLock(new Lock(key), timeout, tryInterval, lockExpireTime, iProcess);
+    }
+
+    /**
+     * 获取锁，自动释放锁
+     *
+     * @param lock 锁
+     * @param timeout 累计获取锁的总时间达到多少后不再尝试，即超时时间（单位毫秒，默认3秒）
+     * @param tryInterval 每隔多少时间重新尝试一次获取（单位毫秒，默认200毫秒）
+     * @param lockExpireTime 获取锁成功后，锁的过期时间（单位毫秒，默认30秒）
+     * @param iProcess 回调，如果不是null，则执行iProcess.process();后自动释放锁
+     * @return 成功失败
+     */
+    public boolean tryLock(Lock lock, Long timeout, Long tryInterval, Long lockExpireTime, IProcess iProcess) {
         if (iProcess == null) {
-            return tryLock(key, timeout, tryInterval, lockExpireTime);
+            return tryLock(lock, timeout, tryInterval, lockExpireTime);
         }
         boolean result = false;
         try {
-            result = tryLock(key, timeout, tryInterval, lockExpireTime);
+            result = tryLock(lock, timeout, tryInterval, lockExpireTime);
             if (result) {
                 iProcess.process();
             }
         } catch (Exception e) {
-            log.error("获取锁执行回调失败,key={}",key, e);
+            log.error("获取锁执行回调失败,key={}",lock.getKey(), e);
             throw e;
         } finally {
+            // 这个锁是不是有可能被别的请求给释放了（没拿到锁也释放锁）
             if (result) {
-                unLock(key);
+                unLock(lock);
             }
         }
         return result;
@@ -112,35 +131,20 @@ public class RedisLock {
      * @return 成功失败
      */
     public boolean tryLock(String key, IProcess iProcess) {
-        return tryLock(key, LOCK_TRY_TIMEOUT, LOCK_TRY_INTERVAL, LOCK_EXPIRE_TIME, iProcess);
+        return tryLock(new Lock(key), LOCK_TRY_TIMEOUT, LOCK_TRY_INTERVAL, LOCK_EXPIRE_TIME, iProcess);
     }
 
-    /**
-     * 获取锁
-     *
-     * @param key 锁的名称KEY
-     * @param lockExpireTime 获取锁成功后，锁的过期时间（单位毫秒，默认30秒）
-     * @return 成功失败
-     */
-    public boolean lock(String key, Long lockExpireTime) {
-        if (StringUtils.isEmpty(key)) {
-            return false;
-        }
-        return coreLock(key, lockExpireTime != null ? lockExpireTime : LOCK_EXPIRE_TIME);
-    }
-
-
-    private boolean coreLock(String key, long lockExpireTime) {
+    private boolean coreLock(Lock lock, long lockExpireTime) {
         Boolean result = null;
         try {
             result = template.execute((RedisCallback<Boolean>) connection ->
-                    connection.set(key.getBytes(StandardCharsets.UTF_8),
-                            new byte[]{1},
+                    connection.set(lock.getKey().getBytes(StandardCharsets.UTF_8),
+                            lock.getValue().getBytes(),
                             Expiration.seconds(lockExpireTime),
                             RedisStringCommands.SetOption.ifAbsent())
             );
         } catch (Exception e) {
-            log.error("redis lock error, key={}", key, e);
+            log.error("redis lock error, key={}", lock.getKey(), e);
         }
         return result != null ? result : false;
     }
@@ -148,14 +152,21 @@ public class RedisLock {
     /**
      * 释放锁
      *
-     * @param key 锁的名称KEY
+     * @param lock 锁
      */
-    public void unLock(String key) {
-        if (StringUtils.isEmpty(key)) {
-            return;
-        }
-        if (Optional.ofNullable(template.hasKey(key)).orElse(false)) {
-            template.delete(key);
+    public void unLock(Lock lock) {
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end ";
+
+        DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptText(script);
+        redisScript.setResultType(Long.class);
+        try {
+            template.execute(redisScript, Collections.singletonList(lock.getKey()), lock.getValue());
+            log.info("释放锁成功的服务器ip为"+ InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            log.warn("获取host异常:{}", e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("锁释放错误信息：" + e.getMessage(), e);
         }
     }
 
